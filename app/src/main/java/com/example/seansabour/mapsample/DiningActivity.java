c1b55f228fc1c6113a347be3f23fecd35095fa60
ritzabzun <maritzayabzun@student.hartnell.edu>
2015-03-14 21:42:17 -0700

    Minimized Menu
    Minimized menu to fit small screens, updated dining menus

--- app/src/main/java/com/example/seansabour/mapsample/DiningActivity.java
+++ app/src/main/java/com/example/seansabour/mapsample/DiningActivity.java
@@ -1,6 +1,7 @@
 package com.example.seansabour.mapsample;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,8 +15,11 @@ public class DiningActivity extends ActionBarActivity implements View.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dining);
 
